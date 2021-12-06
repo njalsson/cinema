d39b85604f9a5e5a7606d8f20928e013a7b3f4fd
aron bergur jóhannsson <aronnemi@gmail.com>
2021-12-06 16:07:45 +0000

    eslint, directories, baseapp, colorpanel

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended'
+    ],
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 13,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+    ],
+    'rules': {
+        'react/no-did-mount-set-state': 2,
+        'react/no-direct-mutation-state': 2,
+        'react/jsx-uses-vars': 2,
+        'no-undef': 2,
+        'semi': [2, 'always'],
+        'react/prop-types': 2,
+        'react/jsx-no-duplicate-props': 2,
+        'indent': [
+            'error',
+            4,
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+    }
+};
