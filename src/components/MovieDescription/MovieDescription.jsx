9e6f65b10181c049471a39a9228a0d5847a33dbf
Daníel Freyr Gylfason <54936265+danielfreyrg@users.noreply.github.com>
2021-12-09 22:03:30 +0000

    Update MovieDescription.jsx

--- src/components/MovieDescription/MovieDescription.jsx
+++ src/components/MovieDescription/MovieDescription.jsx
@@ -1,25 +1,41 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, View } from 'react-native';
 
 import styles from './styles';
 import Genre from '../Genre/Genre';
 
 export default function MovieDescription({realeaseLabel, movie}) {
-    const {title, year, durationMinutes, plot, genres} = movie;
-    const releaseDate = movie["release-dateIS"];
-   
+    const { year, durationMinutes, plot, genres} = movie;
+    const releaseDate = movie['release-dateIS'];
+    const directorsarray = movie['directors_abridged'];
+    const actorsarray = movie['actors_abridged']
+    var moviedirectors = '';
+    for (var i = 0; i < directorsarray.length; i++){
+        moviedirectors += directorsarray[i].name;
+        if (i != directorsarray.length-1){
+            moviedirectors += ', ';
+        }
+    }
+    var movieactors = '';
+    for (var j = 0; j < actorsarray.length; j++){
+        movieactors += actorsarray[j].name;
+        if (j != actorsarray.length-1){
+            movieactors += ', ';
+        }
+    }
+
     return (
         <View style={styles.container}>
             
             <Text style={[styles.text, {marginBottom: 10}]}>{plot}</Text>
-            <View>
-                <Text style={styles.text}>{realeaseLabel} {releaseDate ? releaseDate : year}</Text>
-                {durationMinutes ? 
-                    <Text style={[styles.text,]}>Duration: {durationMinutes} minutes</Text>
-                    : <></>
-                }
-                
-            </View>
+            <Text style={styles.text}>{realeaseLabel} {releaseDate ? releaseDate : year}</Text>
+            {durationMinutes ? 
+                <Text style={[styles.text,]}>Duration: {durationMinutes} minutes</Text>
+                : <></>
+            }
+            <Text style={[styles.text,]}>director: {moviedirectors}</Text>
+            <Text style={[styles.text,]}>Lead actors: {movieactors}</Text>
             <View style={styles.genreContainer}>
                 {genres.map(genre => {
                     if (typeof genre === 'object') {
